bedb31c0ec27cbfefd8f61a091b718c1c31c738f
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2024-11-12 17:31:43 +0000

    Logging DVFS test..

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/phases/common/DVFSInjectionPhase.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/phases/common/DVFSInjectionPhase.java
@@ -16,6 +16,13 @@ import jdk.graal.compiler.nodes.extended.ForeignCallNode;
 import jdk.graal.compiler.phases.BasePhase;
 import jdk.graal.compiler.phases.tiers.HighTierContext;
 import jdk.graal.compiler.nodes.ConstantNode;
+import jdk.graal.compiler.hotspot.meta.joonhwan.BuboCache;
+import jdk.graal.compiler.nodes.*;
+import jdk.graal.compiler.nodes.calc.*;
+import jdk.graal.compiler.phases.tiers.HighTierContext;
+import jdk.graal.compiler.nodes.java.LoadFieldNode;
+import jdk.graal.compiler.nodes.java.StoreFieldNode;
+import jdk.graal.compiler.nodes.java.StoreIndexedNode;
 
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.JavaConstant;
@@ -65,20 +72,44 @@ public class DVFSInjectionPhase extends BasePhase<HighTierContext> {
     @Override
     @SuppressWarnings("try")
     protected void run(StructuredGraph graph, HighTierContext context) {
+       
 
         if (!shouldInstrument(graph)) {
             return;
         }
-        //COMPILATION ID
-        Long id = Long.parseLong(graph.compilationId().toString(Verbosity.ID).split("-")[1]);
-        ValueNode ID = graph.addWithoutUnique(new ConstantNode(JavaConstant.forLong(id), StampFactory.forKind(JavaKind.Long)));
-        
-        ForeignCallNode startTime = graph.add(new ForeignCallNode(JAVA_TIME_MILLIS, ID));
-        graph.addAfterFixed(graph.start(), startTime);
 
-        for (ReturnNode returnNode : graph.getNodes(ReturnNode.TYPE)) {
-            ForeignCallNode javaCurrentCPUtime = graph.add(new ForeignCallNode(JAVA_TIME_MILLIS, ID));
-            graph.addBeforeFixed(returnNode, javaCurrentCPUtime);       
+        try{
+            //COMPILATION ID
+            Long id = Long.parseLong(graph.compilationId().toString(Verbosity.ID).split("-")[1]);
+            ValueNode ID = graph.addWithoutUnique(new ConstantNode(JavaConstant.forLong(id), StampFactory.forKind(JavaKind.Long)));
+            
+            ForeignCallNode dvfsTest = graph.add(new ForeignCallNode(DVFS_TEST));
+            graph.addAfterFixed(graph.start(), dvfsTest);
+
+            LoadFieldNode readBuffer = graph.add(LoadFieldNode.create(null, null, context.getMetaAccess().lookupJavaField(BuboCache.class.getField("Buffer"))));
+            graph.addAfterFixed(dvfsTest, readBuffer);
+            //Read Pointer of Buffer index
+            LoadFieldNode readPointer = graph.add(LoadFieldNode.create(null, null, context.getMetaAccess().lookupJavaField(BuboCache.class.getField("bufferIndex"))));
+            graph.addAfterFixed(readBuffer, readPointer);
+            //Write to Buffer
+            StoreIndexedNode writeDvfsResult = graph.add(new StoreIndexedNode(readBuffer, readPointer, null, null, JavaKind.Long, dvfsTest));
+            graph.addAfterFixed(readPointer, writeDvfsResult);
+
+            //increment ptr
+            ValueNode oneConstantNode = graph.addWithoutUnique(new ConstantNode(JavaConstant.forInt(1), StampFactory.forKind(JavaKind.Int)));
+            AddNode incrementPointer = graph.addWithoutUnique(new AddNode(readPointer, oneConstantNode));
+
+            StoreFieldNode writePointerBack = graph.add(new StoreFieldNode(null, context.getMetaAccess().lookupJavaField(BuboCache.class.getField("bufferIndex")), incrementPointer));
+            graph.addAfterFixed(writeDvfsResult, writePointerBack);
+
+
+
+            for (ReturnNode returnNode : graph.getNodes(ReturnNode.TYPE)) {
+                ForeignCallNode javaCurrentCPUtime = graph.add(new ForeignCallNode(DVFS_TEST));
+                graph.addBeforeFixed(returnNode, javaCurrentCPUtime);       
+            }
+        } catch (Throwable e){
+            throw new RuntimeException("Instrumentation failed" + e.getMessage(), e);
         }
     }
 }
