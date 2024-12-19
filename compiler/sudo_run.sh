3d8b51360bce4a3f6b3d25fb554c478759870a37
spoonjoon <jyoo45@binghamton.edu>
2024-12-19 01:33:11 +0000

    adding sudo_run script

---
+++ compiler/sudo_run.sh
@@ -0,0 +1,38 @@
+#! bin/bash
+# # mx vm -Dgraal.Dump=:1 FibTest
+# mx -J-Djava.library.path=/workspace/graal/vincent vm \
+#     -Dgraal.Dump=:1 \
+#     --add-opens jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.joonhwan=ALL-UNNAMED \
+#     -javaagent:../joonhwan/agent-joon.jar \
+#     -jar ../dacapo/dacapo-9.12-bach.jar sunflow
+
+## Run using an agent that initializes the buffer
+
+sudo -E env "PATH=$PATH" mx -J-Djava.library.path=/workspace/graal/vincent --java-home /openjdk-21/build/linux-x86_64-server-release/images/jdk vm -Dgraal.EnableDVFS=true -Dgraal.SampleRate=1000 \
+    -Xmx10g \
+    --add-opens jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.joonhwan=ALL-UNNAMED \
+    -javaagent:../joonhwan/agent-joon.jar \
+    -jar ../dacapo/dacapo-9.12-bach.jar -n 1 sunflow
+
+
+# mx -J-Djava.library.path=/workspace/graal/vincent vm \
+#     -XX:+PrintCompilation \
+#     -Xmx10g \
+#     -Dgraal.Dump=:1 \
+#     --add-opens jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.joonhwan=ALL-UNNAMED \
+#     -javaagent:../joonhwan/agent-joon.jar \
+#     FibTest
+
+
+# mx -J-Djava.library.path=/workspace/graal/vincent vm \
+#     -Xmx10g \
+#     --add-opens jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.joonhwan=ALL-UNNAMED \
+#     -javaagent:../joonhwan/agent-joon.jar \
+#     FibTest
+
+# mx vm -Xmx10g  \
+#  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI \
+#   -Dgraal.CompilationFailureAction="Print" \
+#   -XX:+UseJVMCICompiler  \
+#   jdk.internal.vm.compiler/org.graalvm.compiler.hotspot.meta.joonhwan=ALL-UNNAMED \
+#   FibTestd
