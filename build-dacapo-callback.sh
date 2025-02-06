44298f8bf5c38ead12d40b9a14772229f7a35017
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2025-02-06 02:35:34 +0000

    build using dacapo version from eflect

--- build-dacapo-callback.sh
+++ build-dacapo-callback.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+EFLECT_HOME=/workspace/eflect
+EFLECT_EXPERIMENTS=$EFLECT_HOME/experiments
+DEPS_DIR=$EFLECT_EXPERIMENTS/resources/jar
 
-javac -cp .:./dacapo/dacapo-9.12-bach.jar \
+DEPS_CP="$DEPS_DIR/dacapo.jar"
+
+javac -cp .:$DEPS_CP \
        joonhwan/dacapo_callback/EnergyCallback.java \
        joonhwan/dacapo_callback/RaplPowercap.java
 
-jar cvf energy-callback.jar \
+jar cvf joonhwan/energy-callback.jar \
        joonhwan/dacapo_callback/EnergyCallback.class \
        joonhwan/dacapo_callback/RaplPowercap.class
