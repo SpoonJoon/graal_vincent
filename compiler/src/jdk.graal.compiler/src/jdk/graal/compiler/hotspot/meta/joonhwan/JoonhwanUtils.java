2b5167c6faad796dd46ddecba3aafa39633de33e
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2025-04-17 20:58:23 +0000

    comment JUtils

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/meta/joonhwan/JoonhwanUtils.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/meta/joonhwan/JoonhwanUtils.java
@@ -1,30 +1,30 @@
-package jdk.graal.compiler.hotspot.meta.joonhwan;
+// package jdk.graal.compiler.hotspot.meta.joonhwan;
 
-import jdk.graal.compiler.hotspot.meta.HotSpotHostForeignCallsProvider.INIT_SYSFS_FILES;
-import jdk.graal.compiler.hotspot.meta.HotSpotHostForeignCallsProvider.CLEANUP_SYSFS_FILES;
+// import jdk.graal.compiler.hotspot.meta.HotSpotHostForeignCallsProvider.INIT_SYSFS_FILES;
+// import jdk.graal.compiler.hotspot.meta.HotSpotHostForeignCallsProvider.CLEANUP_SYSFS_FILES;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
- public class JoonhwanUtils extends Thread {
-
-        public static volatile int bufferIndex = 0;
-        public static volatile int sampleCounter = 0;
-        public static List<String> methodList = new ArrayList<>();
+// import java.util.ArrayList;
+// import java.util.List;
 
 
-        public static void initMethodList(List<String> methods) {
-                methodList.clear();
-                methodList.addAll(methods);
-                System.out.println("DVFS Candidate Method List Initialized: " + methodList);
-        }
+//  public class JoonhwanUtils extends Thread {
 
-        //TODO add new foreign calls
-        public static void initDVFS(){
+//         public static volatile int bufferIndex = 0;
+//         public static volatile int sampleCounter = 0;
+//         public static List<String> methodList = new ArrayList<>();
+
+
+//         public static void initMethodList(List<String> methods) {
+//                 methodList.clear();
+//                 methodList.addAll(methods);
+//                 System.out.println("DVFS Candidate Method List Initialized: " + methodList);
+//         }
+
+//         //TODO add new foreign calls
+//         public static void initDVFS(){
                 
-        }
+//         }
 
-        public static void cleanupDVFS(){}
+//         public static void cleanupDVFS(){}
         
-}
+// }
